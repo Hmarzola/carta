115d6bde5872a19f653e8c1bf4f0e9b65127e261
hernan <hernanbarrios.solucionesviba@gmail.com>
2025-08-29 16:59:39 -0500

    feat: Implement Romantic Love Page with password protection and love letter display

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import RomanticLovePage from "../teAmo.tsx";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RomanticLovePage />
-  </React.StrictMode>
-);
